76e4f7be44a6821182cb892126340c1e7c26f2de
Joanna Turolska <joannaturolska@Joannas-MacBook-Pro.local>
2021-06-17 13:38:34 -0400

    Debug Part2

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ server.use(express.static('public'));
 
 var RestaurantSearch = require('./RestaurantSearch.js');
 let search = new RestaurantSearch();
+
+//setting the port.
+server.set('port', process.env.PORT || 3000);
+
+//Adding routes
+server.get('/',(request,response)=>{
+ response.sendFile(__dirname + '/public/maps.html');
+
+});
+//
+//server.get('/',(request,response)=>{
+// response.sendFile(__dirname + '/map_code.js');
+//
+//});
