a407dbdaff9d7d1d38df46568c2600c9e4886178
Joanna Turolska <joannaturolska@Joannas-MacBook-Pro.local>
2021-06-17 13:30:59 -0400

    added jshitrc file for warnings

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //server.js
 const express = require('express'),
-      server = express()
+      server = express();
 
 //const app = express();
 //app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
